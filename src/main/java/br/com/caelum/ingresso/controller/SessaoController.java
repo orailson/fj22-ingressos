c4683d8db4398f6eb7ccf3192183b1862297129a
orailson <orailson@gmail.com>
2021-03-10 21:55:46 -0300

    Incluido função de adição de sessão

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -1,13 +1,20 @@
 package br.com.caelum.ingresso.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.Sessao;
 import br.com.caelum.ingresso.model.form.SessaoForm;
 
 @Controller
@@ -18,9 +25,12 @@ public class SessaoController {
 	
 	@Autowired
 	private FilmeDao filmeDao;
+	
+	@Autowired
+	private SessaoDao sessaoDao;
 
 	@GetMapping("/admin/sessao")
-	private ModelAndView form(@RequestParam("salaId") Integer salaId, SessaoForm form) {
+	public ModelAndView form(@RequestParam("salaId") Integer salaId, SessaoForm form) {
 		
 		ModelAndView modelAndView = new ModelAndView("sessao/sessao");
 		
@@ -29,5 +39,17 @@ public class SessaoController {
 		modelAndView.addObject("form", form);
 		
 		return modelAndView;
+	}
+	
+	@PostMapping(value = "/admin/sessao")
+	@Transactional
+	public ModelAndView salva(@Valid SessaoForm form, BindingResult result) {
+		if(result.hasErrors()) return form(form.getSalaId(), form);
+		
+		Sessao sessao = form.toSessao(salaDao, filmeDao);
+		
+		sessaoDao.save(sessao);
+		
+		return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
 	}
 }
